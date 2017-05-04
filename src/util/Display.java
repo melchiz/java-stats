cc6700d5aaa3757f276e4f520f5b782ef69c84e8
Melchizo <Melchizo@Melchiz>
2017-05-04 08:47:45 +0300

    Created Display util class

--- src/util/Display.java
+++ src/util/Display.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package util;
 
 /**
  *
  * @author Melchizo
  */
-public class Test {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("This is a test!");
-    }
+public class Display {
     
 }
